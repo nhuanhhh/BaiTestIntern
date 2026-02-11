f57538d7f41d77ce506ac4439aebfff6920f70e5
Như Anh <156521154+Ree2509@users.noreply.github.com>
2026-02-11 09:40:16 +0700

    Updateee

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 8386;
 
 
-app.use(cors());
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
